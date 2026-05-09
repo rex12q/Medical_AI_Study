f3b9b74b5077626ce021e8b3db2ec14588adbad5
Hello:D <angelwldus@gmail.com>
2026-05-09 19:49:12 +0900

    예제23.오차 행렬표 생성 모델의 ->한계점을 분석 및 검토

--- Medical.AI_Latest.Ver/예제23.AI가_발견한_신종_대사증후군_그룹생성.py
+++ Medical.AI_Latest.Ver/예제23.AI가_발견한_신종_대사증후군_그룹생성.py
@@ -15,6 +15,8 @@ from xgboost import XGBClassifier
 from sklearn.cluster import KMeans
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.metrics import confusion_matrix
+#confusion_matrix: 오차 행렬을 나타낼 수 있는 사이킷런.지표 도구 중 하나
 #sav,csv 
 sav_file='training.sav'
 csv_file='csv/[csv]예제23.csv'
@@ -80,10 +82,9 @@ clu_color={
     'High Risk':'red'
 }
 csv_load['Cluster_re']=csv_load['Cluster_col'].map(clu_name)
-def showgraph():
-    sns.scatterplot(x='SBP',y='GLU',hue='Cluster_re',data=csv_load,style='Cluster_re',palette=clu_color) #palette:사용자 커스텀 셋 맞춰주기
-    plt.legend(title='New Groups')
-    plt.show()
+# def showgraph():
+sns.scatterplot(x='SBP',y='GLU',hue='Cluster_re',data=csv_load,style='Cluster_re',palette=clu_color,ax=axes[0]) #palette:사용자 커스텀 셋 맞춰주기
+axes[0].legend(title='New Groups')
 #XGBoost
 X_gb=csv_load[['Gender','Age','HT','WT','BMI','WC']]
 Y_gb=csv_load['Cluster_col']
@@ -133,4 +134,31 @@ test_score=tuning_xgb.score(X_test_gb,Y_test_gb) #실제 모델 성능
 print('-'*50)
 print('Model accuracy result')
 print(f'Best Cross Validate score: {best_score*100:.1f}')
-print(f'Model Accuracy: {test_score*100:.1f}') 
+print(f'Model Accuracy: {test_score*100:.1f}')
+#Confusion Matrix
+print('Drawing confusion_matrix...')
+y_pred=tuning_xgb.predict(X_test_gb)#성능 테스트를 위한 실제 문제 풀기
+confusion_test=confusion_matrix(Y_test_gb,y_pred)
+con_names=['Danger','Healthy','High Risk'] #0,1,2이름 맞춰 쓰기(3*3행렬표)
+sns.heatmap(
+    confusion_test,annot=True,fmt='d',cmap='Blues',ax=axes[1],
+    xticklabels=con_names,yticklabels=con_names #각 칸마다 이름 부여(ticklabels)
+    )
+#heatmap:데이티 사이의 상관관계나 수치 강도를 색상으로 시각화할 때 쓰는 도구(얼마나 연관성이 있는 지를 색을 통해 제공하는 시각적 도구)
+#annot:칸 안에 숫자를 기입하고 싶을 때|fmt(format:형식)='d'(Decimal Integer:10진수 정수),소수점까지 나타내고 싶으면 '.1f'
+axes[1].set_xlabel('Predicted My AI Model') #인공지능이 예측한 것
+axes[1].set_ylabel('Actual (y_pred)') #실제 정답
+axes[1].set_title('Confusion Matrix Result')
+plt.tight_layout()
+plt.show()
+print('End!')
+#오차 행렬표 의미와 해석
+#색의 진함:93이라는 수치를 띄고 있는 칸은 사람이 가장 몰렸다는 뜻
+#정답 대각선(왼쪽 위에서 오른쪽 아래):정답 대각선을 기준으로 해서 보면 색을 가장 진하게 띄고 있는 93, 정확도가 가장 높다는 뜻(참고로 칸 하나하나는 그저 사람이 많다는 의미로 해석)
+#나머지 칸(오답 구역):대각선을 벗어난 칸들의 색이 짙어진다면, AI가 답을 많이 틀렸다는 뜻(심각한 문제)
+#1번째 줄(Danger:중위험):중위험 단계에선 [Danger:0,Healthy:9,High Risk:1] 결과가 나왔으며, 모델한테 어느 정도의 손실점수를 부여->모델 성능 손실
+#2번째 줄(Healthy:건강):건강 단계에선 [Danger:0,Healthy:93,High Risk:9] 결과가 나왔으며, 모델한테 많은 향상점수를 부여->모델 성능 향상
+#3번째 줄(High Risk:고위험):고위험 단계에선 [Danger:0,Healthy:49,High Risk:13] 결과가 나왔으며
+#고위험 단계에서 사람들을 건강이라고 판정한 모델의 성능이 여기서 심각하게 저하된 걸 알 수 있음(고위험에서 43명한테 정상판정을 주었으니..)
+#결론
+#AI는 겉모습 데이터만으로는 숨겨진 병을 찾지 못하며 AI 스크리닝의 한계와 혈액 검사의 필수성이 아주 강력히 필요하다는 것을 알 수 있음
